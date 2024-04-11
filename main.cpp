aa7fc686566a12cfc5ac11913e145b39026df21e
Anup-Nayak <gpnayak1930@gmail.com>
2024-04-11 18:28:21 +0530

    classes initialised

--- main.cpp
+++ main.cpp
@@ -1,22 +1,51 @@
 #include<iostream>
-#include<bits/stdc++.h>
-#include<cmath>
-#include<unordered_map>
+#include<vector>
+#include<algorithm>
+
+// memory errors (invalid read and writes) 10%
+// memory leaks 5%
+// makefile + clean
 
 using namespace std;
 
-void solve(){
-    
-}
+#define ull unsigned long long
+
+struct CacheBlock {
+    bool validBit = false;
+    bool dirtyBit = false;
+    ull tagBits = 0;
+    // unsigned long long lastUsed = 0; 
+};
+
+class Cache {
+    private:
+        ull sets;
+        ull blocks;
+        ull bytes;
+        bool writeAllocate;
+        bool writeThrough;
+        bool fifo;
+
+        vector<vector<CacheBlock>> cache;
+
+    public:
+        Cache(ull sets,ull blocks,ull bytes,bool writeAllocate,bool writeThrough, bool fifo){
+            sets = sets;
+            blocks = blocks;
+            bytes = bytes;
+            writeAllocate = writeAllocate;
+            writeThrough = writeThrough;
+            fifo = fifo;
+
+            cache.resize(sets, vector<Cache>(blocks));
+        }
+};
+
+
 
 int main(){
 
-    int t;
-    cin >> t;
-
-    while(t--){
-        solve();
-    };
+    
 
     return 0;
 }
