84b5bfa7ba658a61a4eed62623582bb6eac34fb4
Anup-Nayak <gpnayak1930@gmail.com>
2024-04-11 14:00:41 +0530

    first

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cmath>
+#include<unordered_map>
+
+using namespace std;
+
+void solve(){
+    
+}
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    };
+
+    return 0;
+}
